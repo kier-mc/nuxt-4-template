3c8000b3ace06123394c32c90256066f15baf9ea
Kieran <kiermccarth@gmail.com>
2025-09-29 17:04:16 +0100

    refactor: reduce to single call

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -4,8 +4,10 @@
  */
 export default defineEventHandler((event) => {
     if (import.meta.dev) {
-        setHeader(event, "Access-Control-Allow-Origin", "*");
-        setHeader(event, "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        setHeader(event, "Access-Control-Allow-Headers", "Content-Type, Authorization");
+        setHeaders(event, {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        });
     };
 });
