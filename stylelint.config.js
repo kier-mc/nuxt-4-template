8207ade25422c7e8ef2ed2be4cd823f6614365a8
Kieran <kiermccarth@gmail.com>
2026-03-18 16:45:56 +0000

    fix: update config for `^7.0.0`

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,41 +5,16 @@ export default {
         "stylelint-config-standard-scss",
         "stylelint-config-recommended-vue",
         "stylelint-config-recommended-vue/scss",
+        "stylelint-plugin-defensive-css/configs/recommended",
+        "stylelint-plugin-logical-css/configs/recommended",
     ],
     plugins: [
         "stylelint-declaration-block-no-ignored-properties",
-        "stylelint-plugin-defensive-css",
-        "stylelint-plugin-logical-css",
         "stylelint-scss",
     ],
     rules: {
         "declaration-property-value-no-unknown": true,
         "plugin/declaration-block-no-ignored-properties": true,
-        "plugin/use-defensive-css": [
-            true,
-            {
-                "accidental-hover": true,
-                "background-repeat": true,
-                "custom-property-fallbacks": true,
-                "flex-wrapping": true,
-                "scroll-chaining": true,
-                "scrollbar-gutter": true,
-                "vendor-prefix-grouping": true,
-                "severity": "warning",
-            },
-        ],
-        "plugin/use-logical-properties-and-values": [
-            true,
-            {
-                severity: "warning",
-            },
-        ],
-        "plugin/use-logical-units": [
-            true,
-            {
-                severity: "warning",
-            },
-        ],
         "selector-class-pattern": "^(?:[a-z]+(-[a-z0-9]+)*|[a-z]+([A-Z][a-z0-9]*)*)(?:__(?:[a-z]+(-[a-z0-9]+)*))?(?:--(?:[a-z]+(-[a-z0-9]+)*))?$",
     },
 };
