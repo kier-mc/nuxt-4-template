63f8942d00f7e212fb18cb854c162c952ca23a15
Kieran <kiermccarth@gmail.com>
2026-05-18 18:12:45 +0100

    feat: add css property order linter/formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import antfu from "@antfu/eslint-config";
 export default antfu(
     {
         formatters: {
-            css: true,
+            css: false,
         },
         languageOptions: {
             parserOptions: {
