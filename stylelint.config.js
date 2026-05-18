2de9bd9cb5147716a991b396ab9da3f7edb11dff
Kieran <kiermccarth@gmail.com>
2026-05-18 18:13:04 +0100

    feat: configure defensive css/order plugin

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,20 +1,48 @@
 /** @type { import("stylelint").Config } */
 export default {
     extends: [
+        "stylelint-config-clean-order",
         "stylelint-config-standard",
         "stylelint-config-standard-scss",
         "stylelint-config-recommended-vue",
         "stylelint-config-recommended-vue/scss",
+        "@stylistic/stylelint-config",
         "stylelint-plugin-defensive-css/configs/recommended",
         "stylelint-plugin-logical-css/configs/recommended",
     ],
     plugins: [
+        "@stylistic/stylelint-plugin",
         "stylelint-declaration-block-no-ignored-properties",
+        "stylelint-order",
         "stylelint-scss",
     ],
     rules: {
+        "@stylistic/indentation": 4,
         "declaration-property-value-no-unknown": true,
+        "defensive-css/no-fixed-sizes": [
+            true,
+            {
+                "at-rules":
+                    {
+                        "@container": false,
+                        "@media": false,
+                    },
+                "severity": "error",
+            },
+        ],
+        "defensive-css/require-named-grid-lines": null,
         "plugin/declaration-block-no-ignored-properties": true,
         "selector-class-pattern": "^(?:[a-z]+(-[a-z0-9]+)*|[a-z]+([A-Z][a-z0-9]*)*)(?:__(?:[a-z]+(-[a-z0-9]+)*))?(?:--(?:[a-z]+(-[a-z0-9]+)*))?$",
     },
+    overrides: [
+        {
+            files: [
+                "./app/assets/styles/index.scss",
+                "./app/assets/styles/reset.scss",
+            ],
+            rules: {
+                "defensive-css/require-pure-selectors": null,
+            },
+        },
+    ],
 };
