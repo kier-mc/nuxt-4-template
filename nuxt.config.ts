05095359746e79941b9d483eaf938bbf558cf11f
Kieran <kiermccarth@gmail.com>
2026-06-27 18:32:40 +0100

    chore: remove unused alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
-import { fileURLToPath } from "node:url";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    alias: {
-        src: fileURLToPath(new URL("./src", import.meta.url)),
-    },
     compatibilityDate: "2025-07-16",
     css: ["~/assets/styles/index.scss"],
     devtools: { enabled: true },
